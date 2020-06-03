13ed3f231ec00a7e2d75d378621c339afa385336
hertelsi <hertelsi@hs-albsig.de>
2020-06-03 16:44:06 +0200

    Datenbank

--- Coding/WebAnw2Backend/db/Insert.sql
+++ Coding/WebAnw2Backend/db/Insert.sql
@@ -3,3 +3,15 @@ INSERT INTO Film (Titel,Beschreibung,Genre,Trailer,FSK,Dauer,Coverpfad,Videopfad
 INSERT INTO Vorstellung (FilmID,Datum,Uhrzeit) VALUES (1,'31.05.2020','20:15 Uhr');
 
 INSERT INTO Ticket (VorstellungsID,Code) VALUES (1,'Ticket-Code-1');
+
+INSERT INTO SnackTyp VALUES (2,'Nachos', 8.50, 'TExt', '');
+INSERT INTO SnackTyp VALUES (1,'Popcorn', 10.50, 'TExt', '');
+
+INSERT INTO Snack Values (1,1,1,2);
+INSERT INTO Snack Values (2,2,1,5);
+
+INSERT INTO Benutzer VALUES (1,'Lustig','Peter','Peter@gmx.de','PayPal',123,133,1);
+INSERT INTO Benutzer VALUES (2,'Lustig','Markus','Markus@gmx.de','PayPal',123,133,2);
+
+INSERT INTO Chat VALUES (1,1);
+INSERT INTO ChatNachicht VALUES (1,1,1,'NAchicht');
