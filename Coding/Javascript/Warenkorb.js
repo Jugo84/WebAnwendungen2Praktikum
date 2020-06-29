abd9c79ee589bc523b3bfedd3d5a9e8a764e047e
johannes j <juengejo@hs-albsig.de>
2020-06-29 10:15:35 +0200

    Warenkorb berechnen

--- Coding/Javascript/Warenkorb.js
+++ Coding/Javascript/Warenkorb.js
@@ -39,6 +39,174 @@ async function createNewGuestbookEntry() {
     newEntryModal.modal("hide");
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+	function rechne() {
+        console.log("in rechen");
+        var summeBier = 0;
+        var summeNachos = 0;
+        var summePopcorn = 0;
+        var summeSprite = 0;
+        var summeChips = 0;
+        var summeSchokolade = 0;
+        var summeHaribo = 0;
+        var summeErdnüsse = 0;
+        var summeSmarties = 0;
+        var summeCola = 0;
+        var summeFanta = 0;
+        try {
+            var popcorn = document.getElementById('Popcorn');
+            var popcornPreis = window.localStorage.getItem('Popcorn');
+            var summePopcorn = popcorn.valueAsNumber * parseInt(popcornPreis);
+            document.getElementById('PopcornGesamt')
+                .value = summePopcorn;
+         } catch (error) {
+            console.log("nicht vorhanden:"+error);
+         }
+        try {
+            var haribo = document.getElementById('Haribo');
+            var hariboPreis = window.localStorage.getItem('Haribo');
+            var summeHaribo = haribo.valueAsNumber * parseInt(hariboPreis);
+            document.getElementById('HariboGesamt')
+                .value = summeHaribo;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var schokolade = document.getElementById('Schokolade');
+            var schokoladePreis = window.localStorage.getItem('Schokolade');
+            var summeSchokolade = schokolade.valueAsNumber * parseInt(schokoladePreis);
+            document.getElementById('SchokoladeGesamt')
+                .value = summeSchokolade;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var smarties = document.getElementById('Smarties');
+            var smartiesPreis = window.localStorage.getItem('Smarties');
+            var summeSmarties = smarties.valueAsNumber * parseInt(smartiesPreis);
+            document.getElementById('SmartiesGesamt')
+                .value = summeSmarties;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var chips = document.getElementById('Chips');
+            var chipsPreis = window.localStorage.getItem('Chips');
+            var summeChips = chips.valueAsNumber * parseInt(chipsPreis);
+            document.getElementById('ChipsGesamt')
+                .value = summeChips;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var erdnüsse = document.getElementById('Erdnüsse');
+            var erdnüssePreis = window.localStorage.getItem('Erdnüsse');
+            var summeErdnüsse = erdnüsse.valueAsNumber * parseInt(erdnüssePreis);
+            document.getElementById('ErdnüsseGesamt')
+                .value = summeErdnüsse;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var cola = document.getElementById('Cola');
+            var colaPreis = window.localStorage.getItem('Cola');
+            var summeCola = cola.valueAsNumber * parseInt(colaPreis);
+            document.getElementById('ColaGesamt')
+                .value = summeCola;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var sprite = document.getElementById('Sprite');
+            var spritePreis = window.localStorage.getItem('Sprite');
+            var summeSprite = sprite.valueAsNumber * parseInt(spritePreis);
+            document.getElementById('SpriteGesamt')
+                .value = summeSprite;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var fanta = document.getElementById('Fanta');
+            var fantaPreis = window.localStorage.getItem('Fanta');
+            var summeFanta = fanta.valueAsNumber * parseInt(fantaPreis);
+            document.getElementById('FantaGesamt')
+                .value = summeFanta;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var bier = document.getElementById('Bier');
+            var bierPreis = window.localStorage.getItem('Bier');
+            var summeBier = bier.valueAsNumber * parseInt(bierPreis);
+            document.getElementById('BierGesamt')
+                .value = summeBier;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var nachos = document.getElementById('Nachos');
+            var nachosPreis = window.localStorage.getItem('Nachos');
+            var summeNachos = nachos.valueAsNumber * parseInt(nachosPreis);
+            document.getElementById('NachosGesamt')
+                .value = summeNachos;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+            mwst = 19;
+        
+        var summeSnacks = summeSprite + summeSmarties + summeSchokolade + summePopcorn+ summeNachos+ summeHaribo+ summeFanta+ summeErdnüsse+ summeCola+ summeChips + summeBier 
+        document.getElementById('snacksGesamt')
+            .value = summeSnacks;
+
+        var summeRufDerWildnis = 0;
+        var summeBerlinBerlin = 0;
+        var summeJamesBond = 0;
+        var summeInception = 0;
+        try {
+            var rufDerWildnis = document.getElementById('Ruf der Wildnis');
+            var summeRufDerWildnis = rufDerWildnis.valueAsNumber *parseInt(12);
+            document.getElementById('Ruf der WildnisGesamt')
+                .value = summeRufDerWildnis;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var jamesBond = document.getElementById('James Bond');
+            var summeJamesBond = jamesBond.valueAsNumber * parseInt(12);
+            document.getElementById('James BondGesamt')
+                .value = summeJamesBond;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var berlinBerlin = document.getElementById('Berlin Berlin');
+            var summeBerlinBerlin = berlinBerlin.valueAsNumber * parseInt(12);
+            document.getElementById('Berlin BerlinGesamt')
+                .value = summeBerlinBerlin;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        try {
+            var inception = document.getElementById('Inception');
+            var summeInception = inception.valueAsNumber * parseInt(12);
+            document.getElementById('Berlin BerlinGesamt')
+                .value = summeInception;
+        } catch (error) {
+            console.log("nicht vorhanden:"+error);
+        }
+        var summeFilme = summeBerlinBerlin + summeRufDerWildnis + summeJamesBond + summeInception;
+        document.getElementById('filmeGesamt')
+            .value = summeFilme;
+        console.log("mwst: "+mwst.valueAsNumber);
+        console.log("mwst: "+parseInt(mwst));
+        var summe = (summeSnacks + summeFilme) * (1 + parseInt(mwst) /100);
+		summe = summe.toFixed(2);
+		document.getElementById('betrag')
+			.value = summe;
+	}
+	document.getElementById("Warenkorbberechnen")
+		.addEventListener("input", rechne);
+});
 
 
 
--- Coding/Javascript/warenkorb_API.js
+++ Coding/Javascript/warenkorb_API.js
@@ -13,6 +13,7 @@ $( document ).ready(function() {
         var $gesamt =$('<table class="table"><thead><tr><th scope="col">#</th><th scope="col">Produkt</th><th scope="col">Menge</th><th scope="col">Preis</th></tr></thead><tbody>');
         nummer=1;
         
+
         for (inhalt in response["daten"]){
             console.log('Inhalt:'+inhalt);
             console.log("hier bin ich auch");
@@ -25,16 +26,18 @@ $( document ).ready(function() {
             console.log(preis);
             var beschreibung = response['daten'][inhalt]['snack']['name'];
             console.log(beschreibung);
+            window.localStorage.setItem(beschreibung,preisEinzeln);
+            console.log("localStorage: "+beschreibung+": "+ window.localStorage.getItem(beschreibung));
             
             
-            var $reihe = $('<tr><th scope="row">'+nummer+'</th><td>'+ beschreibung + '</td><td><input id="'+id+'" type="number" min="0" value="'+ menge +'" class="warenkorb_numbertype"></input></td><td>'+preis+'€</td></tr>')
+            var $reihe = $('<tr><th scope="row">'+nummer+'</th><td>'+ beschreibung + '</td><td><input id="'+beschreibung+'" type="number" min="0" value="'+ menge +'" class="warenkorb_numbertype"></input></td><td><output id="'+beschreibung+'Gesamt" >'+preis+'€</output></td></tr>')
             
             console.log($reihe);
             $($gesamt).append($reihe);
             nummer = nummer +1;
             console.log($gesamt);
         }
-        var $snacks =$('<tr><td></td><td><label for="snacks">Snacks</label></td><td></td><td>'+preisgesamt+'€</td></tr> ')
+        var $snacks =$('<tr><td></td><td><label for="snacks">Snacks</label></td><td></td><td><output id="snacksGesamt" >'+preisgesamt+'€</output></td></tr> ')
         $($gesamt).append($snacks);
         $.ajax({
             url: "http://localhost:8000/api/ticket/gib/BenutzerId/"+id,
@@ -46,15 +49,16 @@ $( document ).ready(function() {
             var preisfilme = 0;
             for (ticket in response["daten"]){
                 var ticketpreis= response['daten'][ticket]['preis'];
-                var filmname = response['daten'][ticket]['film']['film']['titel']
-                var $reiheticket = $('<tr><th scope="row">'+nummer+'</th><td>'+ filmname + '</td><td><input type="number" min="0" value="1" class="warenkorb_numbertype"></input></td><td>'+ticketpreis+'€</td></tr>')
+                var filmname = response['daten'][ticket]['film']['film']['titel'];
+                var $reiheticket = $('<tr><th scope="row">'+nummer+'</th><td>'+ filmname + '</td><td><input type="number" id="'+filmname+'" min="0" value="1" class="warenkorb_numbertype"></input></td><td><output id="'+filmname+'Gesamt" >'+ticketpreis+'€</output></td></tr>')
                 nummer = nummer +1;
                 preisfilme = ticketpreis + preisfilme;
                 $($gesamt).append($reiheticket);
             }
-            var $filme =$('<tr><td></td><td><label for="filme">Filmtickets</label></td><td></td><td>'+preisfilme+'€</td></tr> ')
+            var $filme =$('<tr><td></td><td><label for="filme">Filmtickets</label></td><td></td><td><output id="filmeGesamt" >'+preisfilme+'€</output></td></tr> ')
             $($gesamt).append($filme);
-            var $footer =$('<tr><td></td><td><label for="mwst">MwSt (%)</label></td><td>19%</td><td></td></tr> <tr><td></td><td>Gesamt:</td><td></td><td><output id="betrag" for="stunden satz mwst">0</output> €</td></tr></tbody></table>');     
+            var gesamt = preisgesamt + preisfilme;
+            var $footer =$('<tr><td></td><td><label for="mwst">MwSt (%)</label></td><td>19%</td><td></td></tr> <tr><td></td><td>Gesamt:</td><td></td><td><output id="betrag">'+gesamt+'</output> €</td></tr></tbody></table>');     
             $($gesamt).append($footer);
             $('#warenkorb_reihe_snack').append($gesamt);
         
